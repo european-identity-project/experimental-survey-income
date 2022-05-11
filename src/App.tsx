ae7513f29e710685e2a88f48121ff3b694a2e571
Rodrigo <rodrigofcaba@gmail.com>
2022-05-12 01:53:06 +0200

    Almost finished Beta version

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useCallback } from "react";
 
 // import 'survey-core/survey.min.css';
 import * as SurveyCore from "survey-core";
-import { StylesManager, Model } from "survey-core";
+import { StylesManager, Model, FunctionFactory } from "survey-core";
 import { Survey } from "survey-react-ui";
 import { MODEL } from "./models/eies";
+import { data, eu_data } from "./data/deciles_NAC";
 //@ts-expect-error
 import * as widgets from "surveyjs-widgets";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -16,11 +17,86 @@ import "./App.css";
 StylesManager.applyTheme("defaultV2");
 widgets.nouislider(SurveyCore);
 
+function randomDecile(country: string | any[], eu = false) {
+  let deciles = {};
+
+  if (eu) {
+    deciles = eu_data[country as keyof Object];
+  } else {
+    deciles = data[country as keyof Object];
+  }
+  // Create array of object keys, ["311", "310", ...]
+  if (deciles) {
+    const keys = Object.keys(deciles);
+
+    // Generate random index based on number of keys
+    const randIndex = Math.floor(Math.random() * keys.length);
+
+    // Select a key from the array of keys using the random index
+    const randKey = keys[randIndex];
+
+    // Use the key to get the corresponding name from the "names" object
+    return deciles[randKey as keyof Object]
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+}
+
+function checkDecile(num: number) {
+  let deciles = [];
+  for (const x in data[country as keyof Object]) {
+    const y: any = data[country as keyof Object][x as keyof Object];
+
+    if (y >= num) {
+      deciles.push(data[country as keyof Object][x as keyof Object]);
+    }
+  }
+  if (deciles.length === 1) {
+    return null;
+  } else {
+    var decile = deciles[Math.floor(Math.random() * deciles.length)];
+    return decile.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+}
+
+let country = "";
+
+FunctionFactory.Instance.register("randomDecile", randomDecile);
+
 function App() {
   const survey = new Model(MODEL);
-  survey.focusFirstQuestionAutomatic = false;
-  var question = survey.getQuestionByName("range");
-  question.value = 20;
+  let decile: string | undefined;
+
+  survey.onValueChanged.add(function (survey: any, options: any) {
+    if (options.name !== "country") return;
+    country = options.question.value;
+    // console.log("changes country", country);
+
+    const q = survey.getQuestionByName("objective-income-check-country");
+
+    decile = randomDecile(country);
+
+    q.title = "Do your household earn more or less than " + decile + "?";
+
+    survey.onValueChanged.add(function (survey: any, options: any) {
+      if (options.name !== "objective-income-check-country") return;
+
+      if (decile !== undefined) {
+        const num = parseFloat(decile.replace(/,/g, ""));
+        const page = survey.currentPage;
+        // console.log(page, num);
+        if (checkDecile(num) === null) {
+          return;
+        } else {
+          decile = checkDecile(num) as string;
+          const question = page.addNewQuestion("boolean", "test");
+          question.title =
+            "Do your household earn more or less than " + decile + "?";
+        }
+      }
+    });
+  });
+
   const alertResults = useCallback((sender: { data: any }) => {
     const results = JSON.stringify(sender.data);
     alert(results);
--- src/models/eies.ts
+++ src/models/eies.ts
@@ -1,5 +1,6 @@
 export const MODEL = {
   title: "European Identity Experimental Survey",
+  logoFit: "none",
   logoPosition: "right",
   pages: [
     {
@@ -8,7 +9,7 @@ export const MODEL = {
         {
           type: "html",
           name: "question2",
-          html: '<h1 style="color: #5e9ca0; text-align: center;">Welcome to the European Identity Experimental Survey</h1>\n<p style="text-align: center;"><strong> This is a survey designed to know more about the EU citizen\'s&nbsp; preferences regarding european integration. We appreciate very much your participation.</strong></p>',
+          html: '<h1 style="color: #1ab394; text-align: center;">Welcome to the European Identity Experimental Survey</h1>\n<p style="text-align: center;"><strong> This is a survey designed to know more about the EU citizen\'s&nbsp; preferences regarding european integration. We appreciate very much your participation.</strong></p>',
         },
       ],
       readOnly: true,
@@ -17,27 +18,265 @@ export const MODEL = {
       name: "socioeconomic-background",
       elements: [
         {
-          type: "text",
-          name: "question1",
+          type: "dropdown",
+          name: "country",
+          title: "Country",
+          isRequired: true,
+          choices: [
+            "Austria",
+            "Belgium",
+            "Bulgaria",
+            "Croatia",
+            "Cyprus",
+            "Czechia",
+            "Denmark",
+            "Estonia",
+            "Finland",
+            {
+              value: "Frnace",
+              text: "France",
+            },
+            "Germany",
+            "Greece",
+            "Hungary",
+            "Ireland",
+            "Italy",
+            "Latvia",
+            "Lithuania",
+            "Luxemburg",
+            "Malta",
+            "Netherlands",
+            "Poland",
+            "Portugal",
+            "Romania",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+            "Sweden",
+          ],
+          choicesOrder: "asc",
         },
         {
-          type: "nouislider",
-          name: "range",
-          title: "Please range",
-          step: 1,
-          behaviour: 'drag',
-          pipsMode: 'range'
+          type: "boolean",
+          name: "native",
+          visibleIf: "{country} notempty",
+          title: "Are you a native from {country}?",
+          isRequired: true,
+        },
+        {
+          type: "boolean",
+          name: "citizenship",
+          visibleIf: "{native} == false",
+          title: "Do you have the citizenship from {country}?",
+          isRequired: true,
+        },
+        {
+          type: "text",
+          name: "age",
+          title: "Please, can you tell me your age?",
+          isRequired: true,
+          inputType: "number",
+          min: 16,
+        },
+        {
+          type: "radiogroup",
+          name: "sex",
+          title: "Sex",
+          isRequired: true,
+          choices: [
+            {
+              value: "male",
+              text: "Male",
+            },
+            {
+              value: "female",
+              text: "Female",
+            },
+            "Other",
+          ],
+        },
+        {
+          type: "rating",
+          name: "ideology",
+          title:
+            "When talking about politics, the terms left and right are normally used. Considering the following scale that goes from 1 to 10, in which 1 means “furthest to the left” and 10 means “furthest to the right”, where would you place yourself?",
+          isRequired: true,
+          rateMax: 10,
+          minRateDescription: "Extreme Left",
+          maxRateDescription: "Extreme Right",
+          displayRateDescriptionsAsExtremeItems: true,
+        },
+        {
+          type: "radiogroup",
+          name: "educational-attainment",
+          title:
+            "What is the highest level of education your mother successfully completed?",
+          description:
+            "*NOTE:* These categories are just an example, they might be extended using real ISCED occupations",
+          isRequired: true,
+          choices: [
+            {
+              value: "01",
+              text: "No education",
+            },
+            {
+              value: "02",
+              text: "Primary education",
+            },
+            {
+              value: "03",
+              text: "Secondary Education",
+            },
+            {
+              value: "04",
+              text: "Tertiary education",
+            },
+          ],
+          otherText: "Answer",
+        },
+        {
+          type: "boolean",
+          name: "unemployed",
+          title: "Please, can you tell me if you are unemployed at the moment?",
+          isRequired: true,
+        },
+        {
+          type: "radiogroup",
+          name: "occupation",
+          visibleIf: "{unemployed} = false",
+          title:
+            "Which of the descriptions below best describes the sort of work you do?",
+          description:
+            "*NOTE:* These categories are just an example, they might be extended using real ISCO occupations",
+          isRequired: true,
+          choices: [
+            {
+              value: "01",
+              text: "Professional and technical occupations",
+            },
+            {
+              value: "02",
+              text: "Higher administrator occupations",
+            },
+            {
+              value: "03",
+              text: "Clerical occupations",
+            },
+            {
+              value: "04",
+              text: "Sales occupations",
+            },
+            {
+              value: "05",
+              text: "Service occupations",
+            },
+            {
+              value: "06",
+              text: "Skilled worker ",
+            },
+            {
+              value: "07",
+              text: "Semi-skilled worker",
+            },
+            {
+              value: "08",
+              text: "Unskilled worker",
+            },
+            {
+              value: "09",
+              text: "Farm worker",
+            },
+          ],
+          otherText: "Answer",
+        },
+        {
+          type: "text",
+          name: "ue-kownledge",
+          title: "How many countries currently belong to the European Union?",
+          description: "Please, answer with a number",
+          isRequired: true,
+          inputType: "number",
+          min: 0,
         },
       ],
+      title: "Socioeconomic background",
+      description:
+        "In this section we ask you some questions related to your socioeconomic characteristics",
+      questionsOrder: "initial",
+    },
+    {
+      name: "subjective-income",
+      elements: [
+        {
+          type: "text",
+          name: "question5",
+          title:
+            "Considering the EU is currently formed by 27 Member States, what position do you think {country} holds in a ranking from richest (1) to poorest (27) country?",
+          isRequired: true,
+          inputType: "number",
+          min: 1,
+          max: 27,
+          step: 1,
+        },
+        {
+          type: "text",
+          name: "subjective-income-1",
+          title:
+            "What percentage of households in {country} do you think earn less than yours?",
+          isRequired: true,
+        },
+        {
+          type: "text",
+          name: "subjective-income-2",
+          title:
+            "What percentage of households in the EU do you think earn less than yours?",
+          isRequired: true,
+        },
+      ],
+      title: "Subjective income",
+      description:
+        "In this section we ask you some questions regarding your income relative to your country and the EU.",
+      questionsOrder: "random",
+    },
+    {
+      name: "objective-income",
+      elements: [
+        {
+          type: "boolean",
+          name: "objective-income-check-country",
+          title: "",
+          isRequired: true,
+        },
+        {
+          type: "boolean",
+          name: "question1",
+          title: "",
+          isRequired: true,
+        },
+      ],
+      title: "Objective income",
+      description:
+        "In this section we ask you about your specific household income compared to your country's households as well as EU households.",
+      questionsOrder: "random",
     },
   ],
+  // calculatedValues: [
+  //   {
+  //     name: "national_decile",
+  //     expression: "randomDecile({country})",
+  //     includeIntoResult: true,
+  //   },
+  //   {
+  //     name: "eu_decile",
+  //     expression: "randomDecile({country}, true)",
+  //     includeIntoResult: true,
+  //   },
+  // ],
+  showPrevButton: false,
   showQuestionNumbers: "off",
   showProgressBar: "top",
   progressBarType: "questions",
   startSurveyText: "Start the survey",
   completeText: "Complete the survey",
   firstPageIsStarted: true,
-  questionsOnPageMode: "questionPerPage",
-  showTimerPanel: "top",
-  widthMode: "responsive",
 };
